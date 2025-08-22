ad18744442a3100b2c021367e535332b6f6a69c9
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-22 21:25:36 +0530

    add files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,10 @@ import Customize from './pages/Customize';
 import Customize2 from './pages/Customize2';
 
 export default function App() {
-  const {userData, setUserData}=useContext(userDataContext)
+  const {userData}=useContext(userDataContext)
   return (
       <Routes>
-       {/* <Route path='/'  element={userData? <Home/>:<Navigate to={'/customize'} />}/> */}
        <Route path='/'  element={(userData?.assistantImage && userData?.assistantName)? <Home/>:<Navigate to={'/customize'} />}/>
-       {/* <Route path='/'  element={<Navigate to={'/customize'} />}/> */}
        <Route path='/signup' element={!userData ?<SignUp/>:<Navigate to={"/"} />}/>
        <Route path='/login' element={!userData ?<Login/>:<Navigate to={"/"} />}/>
        <Route path='/customize' element={userData?<Customize/>:<Navigate to={"/signup"} />}/> 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import App from './App.jsx';
 import UserContext from './context/UserContext.jsx';
-import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
