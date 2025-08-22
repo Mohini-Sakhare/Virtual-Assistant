de7178d46db7b3d400cd68df919c934e5396f37a
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-22 22:49:29 +0530

    Update AuthController.js

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -33,8 +33,8 @@ export const SignUp=async(req, res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:false
+            sameSite:"none",
+            secure:true
         })
        
         return res.status(201).json({message:"User signed in successfully", user})
@@ -71,8 +71,8 @@ export const Login=async(req, res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:false
+            sameSite:"none",
+            secure:true
         })
 
         return res.status(200).json({message:"User Logged in successfully", success:true, user})
