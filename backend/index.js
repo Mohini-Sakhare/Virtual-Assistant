1e3e21082ad72773b2a1cfabc79b709c5b215612
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-22 22:47:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app=express();
 
 app.use(
     cors({
-        origin:['http://localhost:5173'],
+        origin:['https://virtual-assistant-frontend-cop0.onrender.com'],
         //  methods: ['GET','POST'],
         credentials: true,
     })
